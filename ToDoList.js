5d9fb7accffff0d9aba698447797df1296ff44df
Apar Shrestha <shresthaapar.co@gmail.com>
2024-10-12 13:51:17 +0200

    finalized produkt, used linear gradient and finished Final touch

--- ToDoList.js
+++ ToDoList.js
@@ -3,6 +3,10 @@ const inp = document.querySelector('[data-inp]')
 const lists = document.querySelector('[data-tasksContainer]')
 const delButton = document.querySelector('#removeBtn')
 const body = document.querySelector("#bod")
+const dateDiv = document.querySelector('[data-date]')
+const timeDiv = document.querySelector('[data-time]')
+let time = dayjs().format('HH:MM:SS')
+let date = dayjs().format('YYYY-MM-DS')
 let submittedTask =[]
 let task = ""
 let tasks = JSON.parse(localStorage.getItem("lsStuff")) || []
@@ -12,11 +16,18 @@ let SelectedListId =""
 form.addEventListener('submit', (e)=>
 {
             e.preventDefault()
+            if(inp.value.trim() !== '')
+            {
             submittedTask = inp.value
             tasks.push(arrayConverter(submittedTask))
             render()
             inp.value = ''
             save()
+            }
+            else {
+                  alert('Nothing cannot be added')
+            }
+          
 
 })
 
@@ -33,6 +44,12 @@ const render = ()=>
       let Ntask  = document.createElement("div") 
       Ntask.dataset.did = task.id
       Ntask.innerText = task.Content
+      Ntask.style.paddingLeft = "8vw"
+      Ntask.style.paddingTop ="1.5vh"
+      Ntask.style.paddingBottom = "1vh"
+      Ntask.style.backgroundColor = "lightblue"
+      Ntask.style.border = "1px solid grey"
+      Ntask.style.overflow ="hidden"
       lists.appendChild(Ntask)
       if (SelectedIdArray.includes(task.id))
       {
@@ -47,10 +64,8 @@ lists.addEventListener('click', e => {
   if (e.target.tagName.toLowerCase() === "div") {
     SelectedListId = e.target.dataset.did;
     if (SelectedIdArray.includes(SelectedListId)) {
-      // If the task is already selected, remove it from the selection
       SelectedIdArray = SelectedIdArray.filter(id => id !== SelectedListId);
     } else {
-      // If the task is not selected, add it to the selection
       SelectedIdArray.push(SelectedListId);
     }
     save();
@@ -99,6 +114,13 @@ body.addEventListener("keydown", keyCheck = (event)=>{
       }
 })
 
+const dateTime = ()=>
+{
+     let time = dayjs().format('HH:mm:ss')
+      let date = dayjs().format('DD-MM-YYYY')
+      timeDiv.innerHTML = time
+      dateDiv.innerHTML = date
+}
 
-
+setInterval(dateTime,1000)
 window.onload = render()
